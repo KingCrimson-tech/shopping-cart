c41e08a02300fa8337a1a719f2c29577a795598c
Chinmay <chinmayrpatil123@gmail.com>
2025-09-13 19:25:26 +0530

    Styling.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,7 +8,7 @@ const Navigation = ({ totalItems }) => {
     <nav className="navigation">
       <div className="nav-container">
         <Link to="/" className="nav-logo">
-          ShopCart
+          Shop Cart
         </Link>
         <ul className="nav-menu">
           <li className="nav-item">
